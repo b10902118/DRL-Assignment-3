3f631d7ea3e6419864d1c2fdab66aa29a2bb2c0f
b10902118 <115149705+b10902118@users.noreply.github.com>
2025-05-05 22:08:41 +0800

    map_location=torch.device('cpu')

--- student_agent.py
+++ student_agent.py
@@ -55,7 +55,7 @@ class model(nn.Module):
 
 
 q = model(4, 12, False, device)
-q.load_state_dict(torch.load("3954.pth"))
+q.load_state_dict(torch.load("3954.pth", map_location=torch.device('cpu')))
 q.to(device)
 
 
